79c0d4bc94dd1a56c56f0a1bfa73e5395c22b5f8
riturajmallick199-byte <riturajmallick199@gmail.com>
2025-12-15 14:46:26 +0530

    pages/api/pages/api)

--- pages/api/pages/api/ahjs.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json([
-    { state: "California", ahj: "Los Angeles County", code: "2022 CBC" },
-    { state: "Texas", ahj: "Travis County", code: "2021 IRC" },
-    { state: "Florida", ahj: "Miami-Dade", code: "2020 FBC" }
-  ]);
-}
